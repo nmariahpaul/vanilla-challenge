4430aebfdccafddaf86377bdd0ca6e5d8928f955
nmariahpaul <npaul@mtroyal.ca>
2022-07-25 20:22:02 -0600

    Added API to include real data

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature-element");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city-name");
+  cityElement.innerHTML = response.data.name;
+  let windElement = document.querySelector("#wind-speed");
+  windElement.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = `${response.data.weather[0].description}`;
+  let currentIconElement = document.querySelector("#current-temp-icon");
+  currentIconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentIconElement.setAttribute("alt", response.data.weather[0].description);
+}
+let apiKey = "28da7852e0ace951fd98245728509e42";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=paris&appid=${apiKey}&units=metric`;
 
+axios.get(apiUrl).then(displayTemperature);
